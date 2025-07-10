ed4cf2317ba68af9dd23571a2b2930c753ccfc9e
rodolfo-riveros <1001420172@unajma.edu.pe>
2025-07-09 22:33:48 -0500

    roles and permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,75 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
+        //creación de roles de la página administrable
+        $role = Role::create(['name' => 'Administrador']);
+
+        //Creación de permisos
+        Permission::create(['name' =>'admin.home.index'])->syncRoles([$role]);
+
+        Permission::create(['name' =>'admin.categoria.index'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.categoria.store'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.categoria.update'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.categoria.destroy'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.categoria.export-pdf'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.categoria.export-excel'])->syncRoles([$role]);
+
+        Permission::create(['name' =>'admin.proveedor.index'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.proveedor.store'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.proveedor.update'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.proveedor.destroy'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.proveedor.export-pdf'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.proveedor.export-excel'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.proveedor.consultar-ruc'])->syncRoles([$role]);
+
+        Permission::create(['name' =>'admin.producto.index'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.producto.store'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.producto.update'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.producto.destroy'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.producto.export-pdf'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.producto.export-excel'])->syncRoles([$role]);
+
+        Permission::create(['name' =>'admin.compra.index'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.compra.store'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.compra.update'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.compra.destroy'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.compra.export-pdf'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.compra.export-excel'])->syncRoles([$role]);
+
+        Permission::create(['name' =>'admin.compra_detalle.index'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.compra_detalle.store'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.compra_detalle.update'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.compra_detalle.destroy'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.compra_detalle.export-pdf'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.compra_detalle.export-excel'])->syncRoles([$role]);
+
+        Permission::create(['name' =>'admin.cliente.index'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.cliente.store'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.cliente.update'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.cliente.destroy'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.cliente.export-pdf'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.cliente.export-excel'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.cliente.consultar-dni'])->syncRoles([$role]);
+
+        Permission::create(['name' =>'admin.venta.index'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.venta.store'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.venta.imprimir_boleta'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.venta.get-product'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.venta.get-customer'])->syncRoles([$role]);
+
+        Permission::create(['name' =>'admin.venta_detalle.index'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.venta_detalle.export-pdf'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.venta_detalle.export-excel'])->syncRoles([$role]);
+
+        Permission::create(['name' =>'admin.notifications.markAsRead'])->syncRoles([$role]);
+        Permission::create(['name' =>'admin.notifications.markAllAsRead'])->syncRoles([$role]);
+
+        //Creacion de usuarios
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            'name' => 'Rodolfo Riveros Mitma',
+            'email' => 'riveros@gmail.com',
+            'password' => bcrypt('12345678')
+        ])->assignRole('Administrador');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 })->name('home');
 
 Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth', 'verified', 'can:dashboard.view'])
     ->name('dashboard');
 
 Route::middleware(['auth'])->group(function () {
@@ -29,40 +29,113 @@ Route::middleware(['auth'])->group(function () {
 });
 
 Route::prefix('admin')->middleware(['auth'])->group(function () {
-    Route::resource('categoria', CategoriaController::class)->only(['index', 'store', 'update', 'destroy'])->names('admin.categoria');
-    Route::get('categoria/export-pdf', [CategoriaController::class, 'exportPdf'])->name('admin.categoria.export-pdf');
-    Route::get('categoria/export-excel', [CategoriaController::class, 'exportExcel'])->name('admin.categoria.export-excel');
-    Route::resource('proveedor', ProveedorController::class)->only(['index', 'store', 'update', 'destroy'])->names('admin.proveedor');
-    Route::get('proveedor/export-pdf', [ProveedorController::class, 'exportPdf'])->name('admin.proveedor.export-pdf');
-    Route::get('proveedor/export-excel', [ProveedorController::class, 'exportExcel'])->name('admin.proveedor.export-excel');
-    Route::resource('producto', ProductoController::class)->only(['index', 'store', 'update', 'destroy'])->names('admin.producto');
-    Route::get('producto/export-pdf', [ProductoController::class, 'exportPdf'])->name('admin.producto.export-pdf');
-    Route::get('producto/export-excel', [ProductoController::class, 'exportExcel'])->name('admin.producto.export-excel');
-    Route::resource('compra', CompraController::class)->only(['index', 'store', 'update', 'destroy'])->names('admin.compra');
-    Route::get('compra/export-pdf', [CompraController::class, 'exportPdf'])->name('admin.compra.export-pdf');
-    Route::get('compra/export-excel', [CompraController::class, 'exportExcel'])->name('admin.compra.export-excel');
-    Route::resource('compra_detalle', Compra_detalleController::class)->only(['index', 'store', 'update', 'destroy'])->names('admin.compra_detalle');
-    Route::get('compra_detalle/export-pdf', [Compra_detalleController::class, 'exportPdf'])->name('admin.compra_detalle.export-pdf');
-    Route::get('compra_detalle/export-excel', [Compra_detalleController::class, 'exportExcel'])->name('admin.compra_detalle.export-excel');
-    Route::resource('cliente', ClienteController::class)->only(['index', 'store', 'update', 'destroy'])->names('admin.cliente');
-    Route::get('cliente/export-pdf', [ClienteController::class, 'exportPdf'])->name('admin.cliente.export-pdf');
-    Route::get('cliente/export-excel', [ClienteController::class, 'exportExcel'])->name('admin.cliente.export-excel');
-    Route::resource('venta', VentaController::class)->only(['index', 'store'])->names('admin.venta');
-    Route::resource('venta_detalle', Venta_detalleController::class)->only(['index'])->names('admin.venta_detalle');
-    Route::get('venta_detalle/export-pdf', [Venta_detalleController::class, 'exportPdf'])->name('admin.venta_detalle.export-pdf');
-    Route::get('venta/{sale}/imprimir-boleta', [Venta_detalleController::class, 'generateBoletaPrint'])->name('admin.venta.imprimir_boleta');
-    Route::get('venta_detalle/export-excel', [Venta_detalleController::class, 'exportExcel'])->name('admin.venta_detalle.export-excel');
+    Route::resource('categoria', CategoriaController::class)
+        ->only(['index', 'store', 'update', 'destroy'])
+        ->names('admin.categoria')
+        ->middleware('can:admin.categoria.index');
+    Route::get('categoria/export-pdf', [CategoriaController::class, 'exportPdf'])
+        ->name('admin.categoria.export-pdf')
+        ->middleware('can:admin.categoria.export-pdf');
+    Route::get('categoria/export-excel', [CategoriaController::class, 'exportExcel'])
+        ->name('admin.categoria.export-excel')
+        ->middleware('can:admin.categoria.export-excel');
+
+    Route::resource('proveedor', ProveedorController::class)
+        ->only(['index', 'store', 'update', 'destroy'])
+        ->names('admin.proveedor')
+        ->middleware('can:admin.proveedor.index');
+    Route::get('proveedor/export-pdf', [ProveedorController::class, 'exportPdf'])
+        ->name('admin.proveedor.export-pdf')
+        ->middleware('can:admin.proveedor.export-pdf');
+    Route::get('proveedor/export-excel', [ProveedorController::class, 'exportExcel'])
+        ->name('admin.proveedor.export-excel')
+        ->middleware('can:admin.proveedor.export-excel');
+
+    Route::resource('producto', ProductoController::class)
+        ->only(['index', 'store', 'update', 'destroy'])
+        ->names('admin.producto')
+        ->middleware('can:admin.producto.index');
+    Route::get('producto/export-pdf', [ProductoController::class, 'exportPdf'])
+        ->name('admin.producto.export-pdf')
+        ->middleware('can:admin.producto.export-pdf');
+    Route::get('producto/export-excel', [ProductoController::class, 'exportExcel'])
+        ->name('admin.producto.export-excel')
+        ->middleware('can:admin.producto.export-excel');
+
+    Route::resource('compra', CompraController::class)
+        ->only(['index', 'store', 'update', 'destroy'])
+        ->names('admin.compra')
+        ->middleware('can:admin.compra.index');
+    Route::get('compra/export-pdf', [CompraController::class, 'exportPdf'])
+        ->name('admin.compra.export-pdf')
+        ->middleware('can:admin.compra.export-pdf');
+    Route::get('compra/export-excel', [CompraController::class, 'exportExcel'])
+        ->name('admin.compra.export-excel')
+        ->middleware('can:admin.compra.export-excel');
+
+    Route::resource('compra_detalle', Compra_detalleController::class)
+        ->only(['index', 'store', 'update', 'destroy'])
+        ->names('admin.compra_detalle')
+        ->middleware('can:admin.compra_detalle.index');
+    Route::get('compra_detalle/export-pdf', [Compra_detalleController::class, 'exportPdf'])
+        ->name('admin.compra_detalle.export-pdf')
+        ->middleware('can:admin.compra_detalle.export-pdf');
+    Route::get('compra_detalle/export-excel', [Compra_detalleController::class, 'exportExcel'])
+        ->name('admin.compra_detalle.export-excel')
+        ->middleware('can:admin.compra_detalle.export-excel');
+
+    Route::resource('cliente', ClienteController::class)
+        ->only(['index', 'store', 'update', 'destroy'])
+        ->names('admin.cliente')
+        ->middleware('can:admin.cliente.index');
+    Route::get('cliente/export-pdf', [ClienteController::class, 'exportPdf'])
+        ->name('admin.cliente.export-pdf')
+        ->middleware('can:admin.cliente.export-pdf');
+    Route::get('cliente/export-excel', [ClienteController::class, 'exportExcel'])
+        ->name('admin.cliente.export-excel')
+        ->middleware('can:admin.cliente.export-excel');
+
+    Route::resource('venta', VentaController::class)
+        ->only(['index', 'store'])
+        ->names('admin.venta')
+        ->middleware('can:admin.venta.index');
+    Route::resource('venta_detalle', Venta_detalleController::class)
+        ->only(['index'])
+        ->names('admin.venta_detalle')
+        ->middleware('can:admin.venta_detalle.index');
+    Route::get('venta_detalle/export-pdf', [Venta_detalleController::class, 'exportPdf'])
+        ->name('admin.venta_detalle.export-pdf')
+        ->middleware('can:admin.venta_detalle.export-pdf');
+    Route::get('venta/{sale}/imprimir-boleta', [Venta_detalleController::class, 'generateBoletaPrint'])
+        ->name('admin.venta.imprimir_boleta')
+        ->middleware('can:admin.venta.imprimir_boleta');
+    Route::get('venta_detalle/export-excel', [Venta_detalleController::class, 'exportExcel'])
+        ->name('admin.venta_detalle.export-excel')
+        ->middleware('can:admin.venta_detalle.export-excel');
+
     // Ruta para consultar DNI
-    Route::get('cliente/consultar-dni', [ClienteController::class, 'consultarDni'])->name('admin.cliente.consultar-dni');
+    Route::get('cliente/consultar-dni', [ClienteController::class, 'consultarDni'])
+        ->name('admin.cliente.consultar-dni')
+        ->middleware('can:admin.cliente.consultar-dni');
     // Ruta para consultar RUC
-    Route::get('/proveedor/consultar-ruc', [ProveedorController::class, 'consultarRuc'])->name('admin.proveedor.consultar-ruc');
+    Route::get('/proveedor/consultar-ruc', [ProveedorController::class, 'consultarRuc'])
+        ->name('admin.proveedor.consultar-ruc')
+        ->middleware('can:admin.proveedor.consultar-ruc');
     // Ruta para consultar producto por código
-    Route::post('/venta/get-product', [VentaController::class, 'getProduct'])->name('admin.venta.get-product');
+    Route::post('/venta/get-product', [VentaController::class, 'getProduct'])
+        ->name('admin.venta.get-product')
+        ->middleware('can:admin.venta.get-product');
     // Ruta para consultar cliente por DNI
-    Route::post('/venta/get-customer', [VentaController::class, 'getCustomer'])->name('admin.venta.get-customer');
+    Route::post('/venta/get-customer', [VentaController::class, 'getCustomer'])
+        ->name('admin.venta.get-customer')
+        ->middleware('can:admin.venta.get-customer');
     // Rutas para notificaciones
-    Route::post('/notifications/{id}/mark-as-read', [NotificationController::class, 'markAsRead'])->name('admin.notifications.markAsRead');
-    Route::post('/notifications/mark-all-as-read', [NotificationController::class, 'markAllAsRead'])->name('admin.notifications.markAllAsRead');
+    Route::post('/notifications/{id}/mark-as-read', [NotificationController::class, 'markAsRead'])
+        ->name('admin.notifications.markAsRead')
+        ->middleware('can:admin.notifications.markAsRead');
+    Route::post('/notifications/mark-all-as-read', [NotificationController::class, 'markAllAsRead'])
+        ->name('admin.notifications.markAllAsRead')
+        ->middleware('can:admin.notifications.markAllAsRead');
 });
 
 require __DIR__.'/auth.php';
